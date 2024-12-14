b3f7031e838cd1e10cf5c64332db6ac712a10183
FrancescoBenassi <francybenna77@gmail.com>
2024-12-14 16:10:42 +0100

    feat: fixed constructor

--- src/Evento.java
+++ src/Evento.java
@@ -1,14 +1,27 @@
+import java.util.Date;
+
 public class Evento {
 
     private String titolo;
-    private String date;
+    private Date date;
     private int totalSeats;
     private int reservedSeats;
 
-    public Evento(String titolo, String date, int totalSeats){
-        this.titolo = titolo;
-        this.date = date;
-        this.totalSeats = totalSeats;
-        this.reservedSeats = 0;
+    public Evento(String titolo, Date date, int totalSeats) {
+        long today = System.currentTimeMillis();
+        Date dateToday = new Date(today);
+        if (dateToday.before(date) && totalSeats > 0) {
+            this.titolo = titolo;
+            this.totalSeats = totalSeats;
+            this.reservedSeats = 0;
+            this.date = date;
+        } else if(totalSeats > 0){
+            System.out.println("La data inserita è già passata, riprova");
+        } else if(dateToday.before(date)){
+            System.out.println("Il numero totale di posti è inferiore ad 1, riprova");
+        } else {
+            System.out.println("La data inserita è già passata e il numero totale di posti è inferiore ad 1, riprova");
+        }
+
     }
 }
