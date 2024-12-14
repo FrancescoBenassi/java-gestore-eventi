9641c639252d0719b1c90dcff6a18735af713458
FrancescoBenassi <francybenna77@gmail.com>
2024-12-14 15:35:01 +0100

    feat: added constructor

--- src/Evento.java
+++ src/Evento.java
@@ -1,3 +1,14 @@
 public class Evento {
 
+    private String titolo;
+    private String date;
+    private int totalSeats;
+    private int reservedSeats;
+
+    public Evento(String titolo, String date, int totalSeats){
+        this.titolo = titolo;
+        this.date = date;
+        this.totalSeats = totalSeats;
+        this.reservedSeats = 0;
+    }
 }
