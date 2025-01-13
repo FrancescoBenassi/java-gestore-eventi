8ca5b76ead956c252b7d11635699a830d4eec9bf
FrancescoBenassi <francybenna77@gmail.com>
2025-01-13 15:38:40 +0100

    feat ProgrammaEventi: fixed main

--- src/ProgrammaEventi.java
+++ src/ProgrammaEventi.java
@@ -47,10 +47,14 @@ public class ProgrammaEventi {
     }
 
     public static void main(String[] args) {
-        Evento event1 = new Evento("Titolo1", 12, 01, 2028, 200);
-        Evento event2 = new Evento("Titolo2", 2, 10, 2050, 200);
-        Evento event3 = new Evento("Titolo4", 28, 12, 2027, 200);
-        Evento event4 = new Evento("Titolo5", 28, 12, 2027, 200);
+        LocalDate date1 = LocalDate.of(2028, 1, 12);
+        LocalDate date2 = LocalDate.of(2050, 10, 2);
+        LocalDate date3 = LocalDate.of(2027, 12, 28);
+        LocalDate date4 = LocalDate.of(2027, 12, 28);
+        Evento event1 = new Evento("Titolo1", date1, 200);
+        Evento event2 = new Evento("Titolo2", date2, 200);
+        Evento event3 = new Evento("Titolo4", date3, 200);
+        Evento event4 = new Evento("Titolo5", date4, 200);
         ProgrammaEventi programmaEventi = new ProgrammaEventi("Titolo56");
         programmaEventi.addEvent(event1);
         programmaEventi.addEvent(event2);
